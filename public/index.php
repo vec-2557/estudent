ea3607cc6ead3778a4abf3ac62c13941eb956591
sasa-2557 <n.sasa2557@gmail.com>
2014-05-11 15:42:26 +0700

    MPV

--- public/index.php
+++ public/index.php
@@ -1,3 +1,52 @@
 <?php
-   echo "test index file"."<br>";
-   echo "Bangkok";
+
+try {
+
+    //Register an autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    //Create a DI
+    $di = new Phalcon\DI\FactoryDefault();
+    $di->set('db', function(){
+    	return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+    			"host" => "localhost",
+    			"username" => "root",
+    			"password" => "",
+    			"dbname" => "estudent"
+    	));
+    });
+    
+    //Setup the view component
+    //Registering Volt as template engine
+	$di->set('view', function() {
+
+    $view = new \Phalcon\Mvc\View();
+
+    $view->setViewsDir('../app/views/');
+
+    $view->registerEngines(array(
+        ".volt" => 'Phalcon\Mvc\View\Engine\Volt'
+    ));
+
+    return $view;
+});
+
+    //Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function(){
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri('/tutorial/');
+        return $url;
+    });
+
+    //Handle the request
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
+} catch(\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
