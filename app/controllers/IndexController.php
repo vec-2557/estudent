1778c1df9c9fa825a46cdaa67ecc5ba4e8cd242e
vec-2557 <sn.nasit@gmail.com>
2014-05-11 20:43:42 -0700

    edit file control

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,1 +1,13 @@
 <?php
+
+class IndexController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+    	$users=new Users();
+        $this->view->name="Sarawut";
+        $this->view->item=Users::find();
+    }
+
+}
