83451d64da2514e38ee2eaf4fccddb5c5a2f8bf4
chuangsin <chuangsin@gmail.com>
2014-05-12 08:46:22 +0700

    commit IndexController.php

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,1 +1,10 @@
 <?php
+
+class IndexController extends \Phalcon\Mvc\Controller{
+    public function indexAction()
+    {
+    	$user = new Users();
+     	$this->view->items=Users::find();
+    }
+
+}
