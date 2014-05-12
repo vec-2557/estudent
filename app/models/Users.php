9dc9b422b161fe331f157187b9b45d4b3d986a2e
vec-2557 <sn.nasit@gmail.com>
2014-05-11 20:45:58 -0700

    edit user

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,7 +1,9 @@
 <?php
-class Users extends \Phalcon\Mvc\Model {
-	public $id;
-	public $name;
-	public $email;
-	
+
+class Users extends \Phalcon\Mvc\Model
+{
+public function getItems(){
+	$item=array("name1","name2","name3");
+	return $item;
+}
 }
